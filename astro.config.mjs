6c268bf02eaf1f0592ef43f17da893a2ccb87e45
vincejstraub <vincejstraub@gmail.com>
2025-10-19 00:57:03 +0100

    Add Vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   prefetch: true,
+  adapter: vercel(),
 });
